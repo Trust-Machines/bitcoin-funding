2f6faf9a19d5989d471c414dd8b0cbc14e7aa7c7
Niel <hello@nieldeckx.be>
2022-08-11 17:13:52 +0800

    add user and wallet tables + api

---
+++ web/pages/api/user/user.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient, User } from '@prisma/client';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<User | string>
+) {
+  if (req.method === 'POST') {
+    await postHandler(req, res);
+  } else if (req.method === 'GET') {
+    await getHandler(req, res);
+  }
+  res.status(400).json("Unsupported method: " + req.method);
+}
+
+async function getHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<User | string>
+) {
+  try {
+    const { appPrivateKey } = req.query;
+    const prisma = new PrismaClient();
+    const result = await prisma.user.findUniqueOrThrow({
+      where: {
+        appPrivateKey: appPrivateKey as string,
+      }
+    });
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json((error as Error).message);
+  }
+}
+
+async function postHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<User | string>
+) {
+  try {
+    const body = JSON.parse(req.body)
+    const prisma = new PrismaClient();
+    const result = await prisma.user.create({
+      data: {
+        appPrivateKey: body.appPrivateKey,
+        address: body.address,
+        fundingWallet: {
+          connect: {
+            publicKey: body.fundingWallet,
+          }
+        }
+      },
+    });
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json((error as Error).message);
+  }
+}
---
+++ web/pages/api/wallet/wallet.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient, FundingWallet } from '@prisma/client';
+
+import { 
+  NewWallet,
+  createWallet,
+  createWalletXpub
+} from '../../../common/bitcoin/bitcoin-js';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<FundingWallet | string>
+) {
+  if (req.method === 'POST') {
+    await postHandler(req, res);
+  }
+  res.status(400).json("Unsupported method: " + req.method);
+}
+
+async function postHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<FundingWallet | string>
+) {
+  try {
+    const walletResult = createWallet();
+
+    // const xpub = 'xpub6CzDCPbtLrrn4VpVbyyQLHbdSMpZoHN4iuW64VswCyEpfjM2mJGdaHJ2DyuZwtst96E16VvcERb8BBeJdHSCVmAq9RhtRQg6eAZFrTKCNqf';
+    // const createWalletXpubResult = createWalletXpub(xpub, 0);
+
+    const prisma = new PrismaClient();
+    const result = await prisma.fundingWallet.create({
+      data: {
+        publicKey: walletResult.publicKey,
+        address: walletResult.address,
+        privateKey: walletResult.privateKey as string
+      },
+    });
+    result.privateKey = "";
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json((error as Error).message);
+  }
+}
