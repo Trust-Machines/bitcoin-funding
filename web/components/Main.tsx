73cd6af292e577bd352852b969ca224b4d61645f
philipdesmedt <philip.desmedt@gmail.com>
2022-08-16 15:37:15 +0200

    useAccount

--- web/components/Main.tsx
+++ web/components/Main.tsx
@@ -1,6 +1,10 @@
 import { Container } from '@/components/Container'
+import { useAccount } from '@micro-stacks/react';
 
 export function Main() {
+  const account = useAccount();
+  console.log(account);
+
   return (
     <Container>
       <span>Congrats, you're signed in!</span>
