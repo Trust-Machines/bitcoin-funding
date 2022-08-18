fdd39fd69eb0898957fdb5a957691b7009c76919
Niel Deckx <contact@nieldeckx.be>
2022-08-18 13:17:26 +0800

    feat: get btc address from public key in clarity (#6)

--- web/common/bitcoin/bitcoin-js.ts
+++ web/common/bitcoin/bitcoin-js.ts
@@ -1,5 +1,6 @@
 import { ECPair, payments, bip32 } from 'bitcoinjs-lib';
 import { btcNetwork } from '../constants';
+import bs58check from 'bs58check';
 
 export interface NewWallet {
   address: string,
@@ -33,6 +34,16 @@ export function createWalletXpub(xpub: string, index: number): NewWallet {
     'privateKey': undefined
   }
   return result;
+}
+
+export function base58CheckEncode(hash: string): string {
+  var encoded = bs58check.encode(Buffer.from(hash, 'hex'));
+  return encoded;
+}
+
+export function base58CheckDecode(address: string): string {
+  var decoded = bs58check.decode(address)
+  return decoded.toString('hex');
 }
 
 export function publicKeyToAddress(publicKey: string): string {
