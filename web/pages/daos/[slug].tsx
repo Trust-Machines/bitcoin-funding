702fe2e533bdf86e1caf0d2d6ccdc8e5e505152e
philipdesmedt <philip.desmedt@gmail.com>
2022-08-18 13:47:19 +0200

    add full width header image

--- web/pages/daos/[slug].tsx
+++ web/pages/daos/[slug].tsx
@@ -6,6 +6,8 @@ import { findDao } from '@/common/fetchers';
 import { Container } from '@/components/Container'
 import { Loading } from '@/components/Loading'
 
+import { StyledIcon } from '@/components/StyledIcon'
+
 const DaoDetails: NextPage = () => {
   const router = useRouter()
   const { slug } = router.query
@@ -14,7 +16,6 @@ const DaoDetails: NextPage = () => {
 
   useEffect(() => {
     const fetchDao = async () => {
-      console.log('Looking for DAO with slug', slug);
       setDao(await findDao(slug));
 
       setIsLoading(false);
@@ -32,9 +33,146 @@ const DaoDetails: NextPage = () => {
           <Loading />
         </div>
       ) : (
-        <>
-          <span>This is loaded: {dao.name}</span>
-        </>
+        <main className="py-10">
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+            <div className="flex items-center space-x-5">
+              <div>
+                <h1 className="text-2xl font-bold text-gray-900">{dao.name}</h1>
+                <p className="text-sm font-medium text-gray-500">
+                  {dao.about}                  
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+              >
+                Manage DAO
+              </button>
+            </div>
+          </div>
+
+          <div className="mx-auto w-full px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8 mt-4">
+            <img
+              className="w-full"
+              src="https://as1.ftcdn.net/v2/jpg/03/32/69/82/1000_F_332698203_XmQ4jYo8vDPfgeqZ3Ake9xfRMS7ChD15.jpg"
+              alt="Workflow"
+            />
+          </div>
+
+          <div className="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
+            <div className="space-y-6 lg:col-start-1 lg:col-span-2">
+              <section aria-labelledby="applicant-information-title">
+                <div className="bg-white shadow sm:rounded-lg">
+                  <div className="px-4 py-5 sm:px-6">
+                    <h2 id="applicant-information-title" className="text-lg leading-6 font-medium text-gray-900">
+                      Applicant Information
+                    </h2>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                  </div>
+                  <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+                    <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                      <div className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">Application for</dt>
+                        <dd className="mt-1 text-sm text-gray-900">Backend Developer</dd>
+                      </div>
+                      <div className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                        <dd className="mt-1 text-sm text-gray-900">ricardocooper@example.com</dd>
+                      </div>
+                      <div className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
+                        <dd className="mt-1 text-sm text-gray-900">$120,000</dd>
+                      </div>
+                      <div className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">Phone</dt>
+                        <dd className="mt-1 text-sm text-gray-900">+1 555-555-5555</dd>
+                      </div>
+                      <div className="sm:col-span-2">
+                        <dt className="text-sm font-medium text-gray-500">About</dt>
+                        <dd className="mt-1 text-sm text-gray-900">
+                          Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
+                          Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+                          proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                        </dd>
+                      </div>
+                      <div className="sm:col-span-2">
+                        <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+                        <dd className="mt-1 text-sm text-gray-900">
+                          
+                        </dd>
+                      </div>
+                    </dl>
+                  </div>
+                  <div>
+                    <a
+                      href="#"
+                      className="block bg-gray-50 text-sm font-medium text-gray-500 text-center px-4 py-4 hover:text-gray-700 sm:rounded-b-lg"
+                    >
+                      Read full application
+                    </a>
+                  </div>
+                </div>
+              </section>
+            </div>
+
+            <section aria-labelledby="timeline-title" className="lg:col-start-3 lg:col-span-1">
+              <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
+                <h2 id="timeline-title" className="text-lg font-medium text-gray-900">
+                  Timeline
+                </h2>
+
+                {/* Activity Feed */}
+                <div className="mt-6 flow-root">
+                  <ul role="list" className="-mb-8">
+                    <li key='1'>
+                      <div className="relative pb-8">
+                        {false ? (
+                          <span
+                            className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+                            aria-hidden="true"
+                          />
+                        ) : null}
+                        <div className="relative flex space-x-3">
+                          <div>
+                            <span className='bg-blue-500 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'>
+                              <StyledIcon
+                                as="ExclamationCircleIcon"
+                                size={6}
+                                solid={false}
+                                className="text-white"
+                              />
+                            </span>
+                          </div>
+                          <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                            <div>
+                              <p className="text-sm text-gray-500">
+                                SP123.... just funded the DAO
+                              </p>
+                            </div>
+                            <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                              <time dateTime='2022-01-08'>Aug, 2022</time>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="mt-6 flex flex-col justify-stretch">
+                  <button
+                    type="button"
+                    className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    Fund DAO
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
       )}
     </Container>
   )
