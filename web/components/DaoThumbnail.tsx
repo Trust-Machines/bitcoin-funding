6e67735b8972f4250f4b7edccc5535206520d4f0
philipdesmedt <philip.desmedt@gmail.com>
2022-08-18 14:15:31 +0200

    add tailwind aspect ratios

--- web/components/DaoThumbnail.tsx
+++ web/components/DaoThumbnail.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 export const DaoThumbnail: FC = ({ dao }) => {
   return (
     <div className="group relative">
-      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+      <div className="w-full min-h-80 min-w-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
         <img
           src='https://as1.ftcdn.net/v2/jpg/03/32/69/82/1000_F_332698203_XmQ4jYo8vDPfgeqZ3Ake9xfRMS7ChD15.jpg'
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
