068b16166572d0a4b14f5dd277a5b909d86d1aba
Niel <hello@nieldeckx.be>
2022-08-19 10:24:22 +0800

    fix: user registration with address

--- clarity/tests/user-registry_test.ts
+++ clarity/tests/user-registry_test.ts
@@ -16,10 +16,10 @@ Clarinet.test({name: "user registry: initial values",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
 
-    let publicKey = hexToBytes('03edf5ed04204ac5ab55832bb893958123f123e45fa417cfe950e4ece67359ee58');
+    let address = hexToBytes('00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31');
 
     let call = await chain.callReadOnlyFn("user-registry-v1-1", "get-btc-to-stx", [
-      types.buff(publicKey)      
+      types.buff(address)      
     ], deployer.address);
     call.result.expectNone();
 
@@ -29,7 +29,7 @@ Clarinet.test({name: "user registry: initial values",
     call.result.expectNone();
 
     call = await chain.callReadOnlyFn("user-registry-v1-1", "is-btc-registered", [
-      types.buff(publicKey)      
+      types.buff(address)      
     ], deployer.address);
     call.result.expectBool(false);
 
@@ -45,27 +45,27 @@ Clarinet.test({name: "user registry: register user",
     let deployer = accounts.get("deployer")!;
     let wallet_1 = accounts.get("wallet_1")!;
 
-    let publicKey = hexToBytes('03edf5ed04204ac5ab55832bb893958123f123e45fa417cfe950e4ece67359ee58');
+    let address = hexToBytes('00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31');
 
     let block = chain.mineBlock([
       Tx.contractCall("user-registry-v1-1", "register-user", [
-        types.buff(publicKey)      
+        types.buff(address)      
       ], wallet_1.address),
     ]);
     block.receipts[0].result.expectOk().expectBool(true);
 
     let call = await chain.callReadOnlyFn("user-registry-v1-1", "get-btc-to-stx", [
-      types.buff(publicKey)      
+      types.buff(address)      
     ], deployer.address);
     call.result.expectSome().expectPrincipal(wallet_1.address);
 
     call = await chain.callReadOnlyFn("user-registry-v1-1", "get-stx-to-btc", [
       types.principal(wallet_1.address)      
     ], deployer.address);
-    call.result.expectSome().expectBuff(publicKey);
+    call.result.expectSome().expectBuff(address);
 
     call = await chain.callReadOnlyFn("user-registry-v1-1", "is-btc-registered", [
-      types.buff(publicKey)      
+      types.buff(address)      
     ], deployer.address);
     call.result.expectBool(true);
 
@@ -85,18 +85,18 @@ Clarinet.test({name: "user registry: can not register same BTC public key twice"
     let deployer = accounts.get("deployer")!;
     let wallet_1 = accounts.get("wallet_1")!;
 
-    let publicKey = hexToBytes('03edf5ed04204ac5ab55832bb893958123f123e45fa417cfe950e4ece67359ee58');
+    let address = hexToBytes('00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31');
 
     let block = chain.mineBlock([
       Tx.contractCall("user-registry-v1-1", "register-user", [
-        types.buff(publicKey)      
+        types.buff(address)      
       ], wallet_1.address),
     ]);
     block.receipts[0].result.expectOk().expectBool(true);
 
     block = chain.mineBlock([
       Tx.contractCall("user-registry-v1-1", "register-user", [
-        types.buff(publicKey)      
+        types.buff(address)      
       ], deployer.address),
     ]);
     block.receipts[0].result.expectErr().expectUint(30001);
