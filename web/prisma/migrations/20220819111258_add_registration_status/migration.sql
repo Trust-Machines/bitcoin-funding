aed0a89a6a5f720f0c7c4c17a9c1e0c94251ec9f
philipdesmedt <philip.desmedt@gmail.com>
2022-08-19 13:14:58 +0200

    tweak: add RegistrationStatus enum

---
+++ web/prisma/migrations/20220819111258_add_registration_status/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The `registrationStatus` column on the `Dao` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `registrationStatus` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "RegistrationStatus" AS ENUM ('STARTED', 'PROCESSING', 'FINISHED');
+
+-- AlterTable
+ALTER TABLE "Dao" DROP COLUMN "registrationStatus",
+ADD COLUMN     "registrationStatus" "RegistrationStatus" NOT NULL DEFAULT 'STARTED';
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "registrationStatus",
+ADD COLUMN     "registrationStatus" "RegistrationStatus" NOT NULL DEFAULT 'STARTED';
