3eab830d9547e3207e9c006d17d556840191094f
Niel <hello@nieldeckx.be>
2022-08-23 08:51:05 +0800

    fix: btc to sats conversion

--- web/pages/daos/new.tsx
+++ web/pages/daos/new.tsx
@@ -89,7 +89,7 @@ const New: NextPage = () => {
                       autoComplete="raisingAmount"
                       className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-none rounded-l-md sm:text-sm border-gray-300"
                       onChange={handleInputChange}
-                      value={state.raisingAmount}
+                      value={state.raisingAmount * 100000000}
                     />
                     <span className="inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
                       BTC
