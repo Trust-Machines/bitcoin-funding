127bd07bdb34b6f688182be91b450109caf311a5
Niel <hello@nieldeckx.be>
2022-08-23 08:50:56 +0800

    fix: renaming

--- web/components/Main.tsx
+++ web/components/Main.tsx
@@ -35,7 +35,7 @@ export function Main() {
           <>
             {daos.length > 0 ? (
               <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {daos.map(dao => <DaoThumbnail key={dao.publicKey} dao={dao} />)}
+                {daos.map(dao => <DaoThumbnail key={dao.address} dao={dao} />)}
               </div>
             ) : (
               <span>No daos yet...</span>
