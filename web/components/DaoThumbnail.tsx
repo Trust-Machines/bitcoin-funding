2ad058046724b2f9bfe7fd21375af48e325ca51a
Niel <hello@nieldeckx.be>
2022-09-01 12:29:47 +0800

    improve dao thumnail

--- web/components/DaoThumbnail.tsx
+++ web/components/DaoThumbnail.tsx
@@ -6,11 +6,11 @@ export const DaoThumbnail: FC = ({ dao }) => {
     <div className="group relative">
       <div className="w-full min-h-80 min-w-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75">
         <img
-          src='https://as1.ftcdn.net/v2/jpg/03/32/69/82/1000_F_332698203_XmQ4jYo8vDPfgeqZ3Ake9xfRMS7ChD15.jpg'
+          src={`${dao.avatar}`}
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
       </div>
-      <div className="mt-2 flex justify-between truncate">
+      <div className="mt-2 flex justify-between">
         <div>
           <h3 className="text-md text-gray-700">
             <Link href={`/daos/${dao.slug}`}>
@@ -20,7 +20,13 @@ export const DaoThumbnail: FC = ({ dao }) => {
               </>
             </Link>
           </h3>
-          <p className="text-sm text-gray-500">{dao.about}</p>
+          <p className="text-sm text-gray-500">
+            {dao.about.length > 70 ? (
+              dao.about.substring(0, 70) + "..."
+            ) : (
+              dao.about
+            )}
+          </p>
         </div>
       </div>
     </div>
