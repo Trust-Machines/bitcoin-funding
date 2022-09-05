1203c321452b76c4b5ae43ad14efbbb32d9172e7
Niel <hello@nieldeckx.be>
2022-09-05 20:56:44 +0800

    remove unused import

--- web/pages/daos/[slug]/manage.tsx
+++ web/pages/daos/[slug]/manage.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Dao } from '@prisma/client'
