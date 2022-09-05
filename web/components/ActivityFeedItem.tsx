c7535c06c94b8397a5de6712a64f6bce0524070a
Niel Deckx <contact@nieldeckx.be>
2022-09-05 20:59:27 +0800

    fix: layout update dao detail page (#32)

--- web/components/ActivityFeedItem.tsx
+++ web/components/ActivityFeedItem.tsx
@@ -3,31 +3,33 @@ import { StyledIcon } from '@/components/StyledIcon'
 export function ActivityFeedItem({
   icon,
   title,
+  subtitle,
   details
 }) {
   return (
     <li key={title}>
-      <div className="relative pb-8">
-        <div className="relative flex space-x-3 mb-4">
+      <div className="relative flex space-x-3 items-center pb-3">
+        <div>
+          <span className='bg-blue-500 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'>
+            <StyledIcon
+              as={icon}
+              size={6}
+              solid={false}
+              className="text-white"
+            />
+          </span>
+        </div>
+        <div className="min-w-0 flex-1 flex justify-between space-x-4">
           <div>
-            <span className='bg-blue-500 h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'>
-              <StyledIcon
-                as={icon}
-                size={6}
-                solid={false}
-                className="text-white"
-              />
-            </span>
+            <p className="text-sm text-gray-500">
+              {title}
+            </p>
+            <p className="text-xs text-gray-400 -mt-1">
+              {subtitle}
+            </p>
           </div>
-          <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-            <div>
-              <p className="text-sm text-gray-500">
-                {title}
-              </p>
-            </div>
-            <div className="text-right text-sm whitespace-nowrap text-gray-500">
-              <time dateTime='2022-01-08'>{details}</time>
-            </div>
+          <div className="text-right text-sm whitespace-nowrap text-gray-500">
+            <time dateTime='2022-01-08'>{details}</time>
           </div>
         </div>
       </div>
