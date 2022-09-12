9749c9ebac151200fb480aeaba80a844ef772fb2
Niel <hello@nieldeckx.be>
2022-09-12 13:52:11 +0800

    Hide section if no user funds yet

--- web/components/Main.tsx
+++ web/components/Main.tsx
@@ -39,16 +39,14 @@ export function Main({ dehydratedState }) {
           <Loading />
         ) : (
           <>
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">Your Funds</h2>
             {userFunds.length > 0 ? (
               <>
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900">Your Funds</h2>
                 <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                   {userFunds.map(fund => <FundThumbnail key={"user" + fund.address} fund={fund} />)}
                 </div>
               </>
-            ) : (
-              <span>You have no funds yet</span>
-            )}
+            ): null}
 
             <h2 className="text-2xl font-bold tracking-tight text-gray-900 mt-8">Our popular Funds</h2>
             {funds.total > 0 ? (
