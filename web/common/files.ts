b6a7c89d4f5c4c7329662bff1ee75007dd77e416
Niel <hello@nieldeckx.be>
2022-09-12 08:39:25 +0800

    Can still save locally for development purposes

--- web/common/files.ts
+++ web/common/files.ts
@@ -22,24 +22,44 @@ const saveToS3 = async (blob: any, name: string) => {
 export async function saveFile(file: any, name: string) {
   const extension = file.originalFilename.split(".").pop();
   const data = fs.readFileSync(file.filepath);
-  fs.unlinkSync(file.filepath);
 
-  try {
-    const result = await saveToS3(data, `${name}.${extension}`);
-    return result.Location;
-  } catch (e) {
-    console.log(e);
-    return '';
+  if (process.env.AWS_ACCESS_KEY_ID == undefined) {
+    // Development: save locally
+    const directory = `/avatars/${name}.${extension}`
+    fs.writeFileSync(`./public/${directory}`, data);
+    fs.unlinkSync(file.filepath);
+    return directory;
+    
+  } else {
+    // Production: save to S3
+    try {
+      fs.unlinkSync(file.filepath);
+      const result = await saveToS3(data, `${name}.${extension}`);
+      return result.Location;
+    } catch (e) {
+      console.log(e);
+      return '';
+    }
   }
 }
 
 export async function createPlaceholderAndSaveFile(seed: string) {
-  try {
-    const data = createAvatar(style, { seed: seed, bold: true, fontSize: 30 });
-    const result = await saveToS3(data, `${seed}.svg`);
-    return result.Location;
-  } catch (e) {
-    console.log(e);
-    return '';
+  const data = createAvatar(style, { seed: seed, bold: true, fontSize: 30 });
+
+  if (process.env.AWS_ACCESS_KEY_ID == undefined) {
+    // Development: save locally
+    const directory = `/avatars/${seed}.svg`;
+    fs.writeFileSync(`./public/${directory}`, data);
+    return directory;
+
+  } else {
+    // Production: save to S3
+    try {
+      const result = await saveToS3(data, `${seed}.svg`);
+      return result.Location;
+    } catch (e) {
+      console.log(e);
+      return '';
+    }
   }
 }
