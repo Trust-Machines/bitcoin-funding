e76e15256907be153d1a57f09d3c14977ef82fa2
philipdesmedt <philip.desmedt@gmail.com>
2022-10-17 12:08:31 +0200

    feat: show Stacks address when logged in

--- web/components/WalletConnectButton.tsx
+++ web/components/WalletConnectButton.tsx
@@ -1,15 +1,37 @@
 import { useEffect, useState } from 'react'
-import { useAuth } from '@micro-stacks/react';
+import { useAccount, useAuth } from '@micro-stacks/react';
 import { Button } from '@/components/Button';
 import { DownloadWalletModal } from '@/components/DownloadWalletModal';
- 
+
+const shortAddress = (address: string | null) => {
+  if (address) {
+    if (address.includes('.')) {
+      return address;
+    }
+    return `${address.substring(0, 5)}...${address.substring(address.length, address.length - 5)}`;
+  }
+
+  return '';
+};
+
+const useHover = () => {
+  const [hovering, setHovering] = useState(false);
+  const onHoverProps = {
+    onMouseEnter: () => setHovering(true),
+    onMouseLeave: () => setHovering(false),
+  }
+  return [hovering, onHoverProps]
+}
+
 export const WalletConnectButton = ({ buttonText }) => {
   const { openAuthRequest, isRequestPending, signOut, isSignedIn } = useAuth();
   const [label, setLabel] = useState(buttonText);
   const [showDownloadModal, setShowDownloadModal] = useState(false);
+  const account = useAccount();
+  const [buttonHover, buttonHoverProps] = useHover();
 
   useEffect(() => {
-    setLabel(isRequestPending ? 'Loading...' : isSignedIn ? 'Sign out' : buttonText);
+    setLabel(isRequestPending ? 'Loading...' : isSignedIn ? shortAddress(account.stxAddress) : buttonText);
   }, [isRequestPending, isSignedIn]);
 
   return (
@@ -17,7 +39,7 @@ export const WalletConnectButton = ({ buttonText }) => {
       {showDownloadModal ? (
         <DownloadWalletModal showDownloadModal={showDownloadModal} setShowDownloadModal={setShowDownloadModal} />
       ) : null}
-      <Button
+      <Button {...buttonHoverProps}
         onClick={async () => {
           if (isSignedIn) {
             await signOut();
@@ -31,7 +53,24 @@ export const WalletConnectButton = ({ buttonText }) => {
           }
         }}
       >
-        {label}
+        {isSignedIn ? (
+          <span className="flex items-center w-36 ml-3">
+            {buttonHover ? (
+              <span className="flex items-center">
+                <span className="truncate text-md ml-1">Sign Out</span>
+              </span>
+            ) : (
+              <>
+                <span className="block h-2.5 w-2.5 rounded-full bg-green-400 ring-2 ring-white mr-3" />
+                <span className="truncate">{label}</span>
+              </>
+            )}
+          </span>
+        ) : (
+          <>
+            {label}
+          </>
+        )}
       </Button>
     </>
   );
