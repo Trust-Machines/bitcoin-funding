9f27ebbeb8ed57d9d616dc0f59a89e9e20ae937a
Niel Deckx <hello@nieldeckx.be>
2022-10-24 14:28:25 +0800

    chore: cleanup

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from 'next'
-import { useAuth } from '@micro-stacks/react';
-import { useEffect, useState } from 'react'
-
 import { Hero } from '@/components/Hero'
 import { HomeGrid } from '@/components/HomeGrid'
-import { Main } from '@/components/User'
 
 import { getServerSideProps } from '@/common/session/index'
 
