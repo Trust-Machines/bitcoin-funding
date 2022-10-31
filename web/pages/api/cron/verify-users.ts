983f0fb3e28920cc59b67719ba6a3ee759626ad8
Niel Deckx <hello@nieldeckx.be>
2022-10-31 10:38:47 +0800

    chore: extra logging

--- web/pages/api/cron/verify-users.ts
+++ web/pages/api/cron/verify-users.ts
@@ -30,6 +30,7 @@ async function postHandler(
     try {
       let result = await verifyUser(user.address);
       if (result.email && result.registrationStatus == RegistrationStatus.COMPLETED) {
+        console.log("[VERIFY USERS] send email:", result.email);
         sendMailRegistration(result.email);
       }
       console.log("[VERIFY USERS] verification response:", result);
