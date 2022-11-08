362e215c18d7491732a7c462ac6629b5217f50ce
philipdesmedt <philip.desmedt@gmail.com>
2022-11-08 15:45:59 +0100

    done

--- web/pages/funds/[slug].tsx
+++ web/pages/funds/[slug].tsx
@@ -278,6 +278,50 @@ const FundDetails: NextPage = ({ dehydratedState }) => {
                       </dd>
                     </div>
                   </dl>
+
+                  <dl className="grid grid-cols-1 gap-y-4 mt-5">
+                    {isAdmin ? (
+                      <Link
+                        href={`/funds/${fund.slug}/manage`}
+                        className="inline-flex items-center justify-center mt-1 w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-orange-500"
+                      >
+                        Manage
+                      </Link>
+                    ):null}
+
+                    {!isSignedIn ? (
+                      <div className="has-tooltip">
+                        <span className="tooltip rounded shadow-lg p-2 bg-black text-white -mt-8 font-semibold">
+                          Connect your Stacks wallet first... 
+                        </span>
+                        <button
+                          className="inline-flex items-center justify-center disabled:opacity-75 mt-1 w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 focus:outline-none cursor-not-allowed"
+                          disabled
+                        >
+                          Fund this project
+                        </button>
+                      </div>
+                    ) : user.registrationStatus == RegistrationStatus.COMPLETED ? (
+                      <Link
+                        href={`/funds/${fund.slug}/fund`}
+                        className="inline-flex items-center justify-center mt-1 w-full px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-orange-500"
+                      >
+                        Fund this project
+                      </Link>
+                    ): (
+                      <div className="has-tooltip">
+                        <span className="tooltip rounded shadow-lg p-2 bg-black text-white -mt-8 font-semibold">
+                          Your BTC address is being registered on-chain...
+                        </span>
+                        <button
+                          className="inline-flex items-center w-full justify-center disabled:opacity-75 mt-1 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-600 focus:outline-none cursor-not-allowed"
+                          disabled
+                        >
+                          Fund this project
+                        </button>
+                      </div>
+                    )}
+                  </dl>
                 </div>
               </div>
 
